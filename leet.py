50791a812d09454f9cdb2cdcd55b051f47a48c3a
Nate Foss <npfoss@gmail.com>
2019-02-24 18:09:23 -0500

    got it

--- leet.py
+++ leet.py
@@ -1,5 +1,6 @@
 from bitstring import BitArray
 import numpy as np
+from collections import defaultdict
     
 simplealph = ''
 with open('alphabet.csv', 'r') as file:
@@ -8,6 +9,27 @@ with open('alphabet.csv', 'r') as file:
 LEGAL = set('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM. ,?!1234567890()&$\'"-')
 
 bigrams = np.genfromtxt('letter_transition_matrix.csv', delimiter=',')
+
+N = 3
+
+def ngrams():
+    """
+    returns n-gram counts for shakespeare corpus
+    """
+    d = defaultdict(int)
+    s = ''
+    with open('shakespeare.txt', 'r') as f:
+        for line in f:
+            s += line.strip() + ' '
+            for i in range(len(s)-N+1):
+                d[s[i:i+N].lower()] += 1
+            s = s[-N+1:]
+    return d
+
+ngrams = ngrams()
+# print(ngrams)
+# embed()
+# raise
 
 def byteSum(a, b):
     """
@@ -54,17 +76,25 @@ def choosebest(message, c1, c2):
         if message[-1] != ' ':
             return '1'
 
-    # bigram probability
+    # ngram probability
     print('have to use prob:', c1, c2)
-    # if not in alphabet used for bigrams, assume can only come after space or another nonalphabet char
-    if c1.lower() not in simplealph:
-        p1 = 1/len(simplealph) if message[-1] == ' ' or message[-1].lower() not in simplealph else 0
-    else:
-        p1 = bigrams[simplealph.index(message[-1].lower())][simplealph.index(c1.lower())]
-    if c2.lower() not in simplealph:
-        p2 = 1/len(simplealph) if message[-1] == ' ' or message[-1].lower() not in simplealph else 0
-    else:
-        p2 = bigrams[simplealph.index(message[-1].lower())][simplealph.index(c2.lower())]
+
+    # use 3-grams for now
+    p1 = ngrams[(message[-N+1:] + c1).lower()]
+    p2 = ngrams[(message[-N+1:] + c2).lower()]
+
+
+    if p1 == 0 and p2 == 0:
+        print('have to use bigrams... what? should not happen much')
+        # if not in alphabet used for bigrams, assume can only come after space or another nonalphabet char
+        if c1.lower() not in simplealph:
+            p1 = 1/len(simplealph) if message[-1] == ' ' or message[-1].lower() not in simplealph else 0
+        else:
+            p1 = bigrams[simplealph.index(message[-1].lower())][simplealph.index(c1.lower())]
+        if c2.lower() not in simplealph:
+            p2 = 1/len(simplealph) if message[-1] == ' ' or message[-1].lower() not in simplealph else 0
+        else:
+            p2 = bigrams[simplealph.index(message[-1].lower())][simplealph.index(c2.lower())]
     print(c1, p1, c2, p2)
     if p1 > p2:
         return '1'
@@ -93,13 +123,14 @@ bits = readbits()
 
 pad = BitArray(uint=int('0x5a',16), length=8)
 message = str(chr((bits.pop(0) ^ pad).uint))
+path = [(message, pad)]
 for b in bits:
     print('message so far:', message)
     pad = promptpad(message, b, pad)
     message += str(chr((b ^ pad).uint))
+    # path.append((message, pad))
 
 print('"' + message + '"')
 
-from IPython import embed; embed()
 
 
