90fff1fc65d78529e89433e4c801933b42c9c730
Nate Foss <npfoss@gmail.com>
2019-02-25 22:49:44 -0500

    remove unnecessary stuff

--- leet.py
+++ leet.py
@@ -2,13 +2,7 @@ from bitstring import BitArray
 import numpy as np
 from collections import defaultdict
     
-simplealph = ''
-with open('alphabet.csv', 'r') as file:
-    simplealph = file.readline().strip().split(',')
-
 LEGAL = set('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM. ,?!1234567890()&$\'"-')
-
-bigrams = np.genfromtxt('letter_transition_matrix.csv', delimiter=',')
 
 N = 3
 
@@ -27,9 +21,6 @@ def ngrams():
     return d
 
 ngrams = ngrams()
-# print(ngrams)
-# embed()
-# raise
 
 def byteSum(a, b):
     """
@@ -78,23 +69,10 @@ def choosebest(message, c1, c2):
 
     # ngram probability
     print('have to use prob:', c1, c2)
-
     # use 3-grams for now
     p1 = ngrams[(message[-N+1:] + c1).lower()]
     p2 = ngrams[(message[-N+1:] + c2).lower()]
 
-
-    if p1 == 0 and p2 == 0:
-        print('have to use bigrams... what? should not happen much')
-        # if not in alphabet used for bigrams, assume can only come after space or another nonalphabet char
-        if c1.lower() not in simplealph:
-            p1 = 1/len(simplealph) if message[-1] == ' ' or message[-1].lower() not in simplealph else 0
-        else:
-            p1 = bigrams[simplealph.index(message[-1].lower())][simplealph.index(c1.lower())]
-        if c2.lower() not in simplealph:
-            p2 = 1/len(simplealph) if message[-1] == ' ' or message[-1].lower() not in simplealph else 0
-        else:
-            p2 = bigrams[simplealph.index(message[-1].lower())][simplealph.index(c2.lower())]
     print(c1, p1, c2, p2)
     if p1 > p2:
         return '1'
@@ -107,11 +85,6 @@ def promptpad(message, bits, lastpad):
     c = BitArray(uint=int('0x5A', 16), length=8)
     rev = pad
     rev.reverse()
-    # print('options:')
-    # print('1:', str(chr((bits ^ rev).uint)), '<')
-    # print('2:', str(chr((bits ^ byteSum(rev, c)).uint)), '<')
-    # i = input('?:')
-    # print('input was', i)
     i = choosebest(message, str(chr((bits ^ rev).uint)), str(chr((bits ^ byteSum(rev, c)).uint)))
     if i == '1':
         return rev
@@ -123,12 +96,10 @@ bits = readbits()
 
 pad = BitArray(uint=int('0x5a',16), length=8)
 message = str(chr((bits.pop(0) ^ pad).uint))
-path = [(message, pad)]
 for b in bits:
     print('message so far:', message)
     pad = promptpad(message, b, pad)
     message += str(chr((b ^ pad).uint))
-    # path.append((message, pad))
 
 print('"' + message + '"')
 
